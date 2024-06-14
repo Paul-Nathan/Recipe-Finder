5e01961160b61fc13d6beb4fcd5f3df05b4fc60f
Paul-Nathan <153547909+Paul-Nathan@users.noreply.github.com>
2024-06-14 14:23:57 +0100

    Add files via upload
    Final code Output for Recipe-finder named FoodieHub

---
+++ HeroSection.js
@@ -0,0 +1,16 @@
+import CustomImage from "./CustomImage"
+export default function HeroSection){
+    return ( 
+        <div className = "section hero">
+            <div className = "col">
+                <h1 className= "title">What Are We About</h1>
+                <p className="info">RecipeApp is a place where you can please your soul and tummy with delicious food recipes of all cuisine. And our service is absolutely free. So start exploring now. </p>
+                <button className = "btn">explore now</button>
+            </div>
+            <div className= "col">
+                
+            </div>
+
+        </div>
+    )
+}
